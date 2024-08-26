62a3066aa3a85ba74c87256beff1738f81ba346f
davidtranokvip <davidtran@okvip.com>
2024-08-27 00:36:35 +0700

    add theme chakra

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  swcMinify: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'i.ibb.co',
+      'scontent.fotp8-1.fna.fbcdn.net',
+    ],
+    // Make ENV
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
